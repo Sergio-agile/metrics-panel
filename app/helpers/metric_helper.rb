8ef573072af2e1e0f76ec7a52988cf87c28d61b3
Sergio García <sergio.garcia.sanjuan@gmail.com>
2024-09-05 18:13:04 +0200

    chart by hours

--- app/helpers/metric_helper.rb
+++ app/helpers/metric_helper.rb
@@ -1,6 +1,6 @@
 module MetricHelper
 
   def format_timestamp(timestamp)
-    timestamp.strftime("%Y-%m-%d") if timestamp.present?
+    timestamp.strftime("%I:%M %p") if timestamp.present?
   end
 end
